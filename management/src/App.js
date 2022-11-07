c493b777d433ad56ff2f4640c9d3945d60172cec
Youngwok Kim <youngwok.kim83@gmail.com>
2022-11-07 09:01:24 -0500

    first commit

---
+++ management/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="gray-background" alt="logo" />
+        <h1>Let's code some management website</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
